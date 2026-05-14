c708450a6107410596eb482890dae9dd14bfa59c
Gautam <gautambutola@outlook.com>
2026-05-14 21:33:29 +0530

    Add PATCH method to CORS configuration

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(cookieParser())
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true,
+  methods : ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
 }))
 
 app.use(express.json())
